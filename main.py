8f0ae267ee624a6b20a12f901ca50d32dd87151e
timbzz <btymofii301@icloud.com>
2023-10-28 13:53:44 +0300

    now with names

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import checkgroups as cg
 import pandas as pd # робота з таблицею excel
 
+
 #починаєм виводити в streamlit
 st.title ("Пошук груп")
 #surname = st.text_input("Прізвище: ")
@@ -10,23 +11,30 @@ df = df.fillna(0)
 values = df.values.tolist() # перетворює dataframe на list, values -- вся наша таблиця
 values_surname = df.iloc[:, 0].values.tolist() # df.iloc шукає вибирає потрібні значення
 values_name = df.iloc[:, 1].values.tolist()
-
+column_names = df.columns.tolist()
 #забирає від прізвищ пробіли
 for i in range(len(values_surname)):
         values_surname[i] = values_surname[i].strip()
-surname = st.selectbox("Прізвище: ", values_surname)
-column_names = df.columns.tolist()
-
+#surname = st.selectbox("Прізвище: ", values_surname)
 
 def find_surname(surname):
     i = 0
-    while surname != values_surname[i]:
+    while surname != values_nameAndSurname[i]:
         i+=1
     return (i)
 
+comnames = []
+def combine (sur, nam):
+    for i in range (len(sur)):
+        comnames.append(sur[i] + ' ' + nam[i])
+    return comnames
 
-if not surname in values_surname:
-        st.write("Неправильно введене прізвище")
+
+values_nameAndSurname = combine(values_surname, values_name)
+surname = st.selectbox("Імʼя", values_nameAndSurname, key = 'nameOfTheStudent')
+
+if not surname in values_nameAndSurname:
+        st.write("Неправильно введене імʼя")
 elif surname != '':
     index_student = find_surname(surname)
 find_groups_names_array = (cg.find_groups(index_student,column_names,values,0))
--- playground.py
+++ playground.py
@@ -9,10 +9,71 @@ import main
 #    submitButton = st.form_submit_button(label = 'Показати')
 sur_list = main.values_surname
 name_list = main.values_name
-surname = st.selectbox("Прізвище", sur_list)
+surname = st.selectbox("Прізвище", sur_list, key = 'surnameOfTheStudent')
 names = []
 for i in range(len(sur_list)):
     if surname == sur_list[i]:
         names.append(name_list[i])
-if len(names) == 1:
-    name = st.selectbox("Імʼя", [names])
+if len(names) != 1:
+
+    name = st.selectbox("Імʼя", names, key = 'nameOfTheStudent')
+
+
+
+
+'''
+import streamlit as st
+import checkgroups as cg
+import pandas as pd # робота з таблицею excel
+
+#починаєм виводити в streamlit
+st.title ("Пошук груп")
+#surname = st.text_input("Прізвище: ")
+df = pd.read_excel("https://docs.google.com/spreadsheets/d/e/2PACX-1vT2aEeS8hEAxrMseZiHx56A80ypA9BKjXxSLStQ9T5KEh6CMhqLPswQ5pB-GpyblSvvM2U0sTMJpaCj/pub?output=xlsx", sheet_name='Students')
+df = df.fillna(0)
+values = df.values.tolist() # перетворює dataframe на list, values -- вся наша таблиця
+values_surname = df.iloc[:, 0].values.tolist() # df.iloc шукає вибирає потрібні значення
+values_name = df.iloc[:, 1].values.tolist()
+column_names = df.columns.tolist()
+#забирає від прізвищ пробіли
+for i in range(len(values_surname)):
+        values_surname[i] = values_surname[i].strip()
+#surname = st.selectbox("Прізвище: ", values_surname)
+
+def find_surname(surname, name):
+    i = 0
+    while surname != values_surname[i] and name != values_name[i]:
+        i+=1
+    return (i)
+
+
+
+name_list = values_name
+surname = st.selectbox("Імʼя", values_surname, key = 'surnameOfTheStudent')
+#surname = 'Кравченко'
+names = []
+"""
+for i in range(len(values_surname)):
+    if surname == values_surname[i]:
+        names.append(name_list[i])
+        if len(names) == 1:
+             surname_index = i
+if len(names) != 1:
+    #name = input("Name: ")
+    name = st.selectbox("Імʼя", names, key = 'nameOfTheStudent')
+else:
+     name = values_name[surname_index]
+"""
+
+
+if not surname in values_surname:
+        st.write("Неправильно введене прізвище")
+elif surname != '' and name != '':
+    index_student = find_surname(surname, name)
+find_groups_names_array = (cg.find_groups(index_student,column_names,values,0))
+find_groups_values_array = (cg.find_groups(index_student,column_names,values,1))
+
+for i in range(len(find_groups_names_array)):
+      st.write(f'{find_groups_names_array[i]}: {find_groups_values_array[i]}')
+
+'''
